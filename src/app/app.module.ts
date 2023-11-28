fa2b3620ae4b2cfcbcdcba4b62492e4034954008
prithish <prithish.shan@gmail.com>
2023-11-27 21:17:12 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, NgZone } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import confetti from 'canvas-confetti';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.css'
+})
+export class GameComponent {
+  moreInfoData: any = {exists: false};
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer, private cdr:ChangeDetectorRef) { }
+
+  ngOnInit() {
+    setInterval(() => this.pollData(), 1000)
+    this.cdr.detach();
+    this.cdr.detectChanges();
+  }
+
+  pollData = () => {
+    this.http.get("http://localhost:8080/more_info").subscribe(
+      (result:any) => {
+        if (result.title) {
+          if (this.moreInfoData.id != result.id) {
+            if (result.isCase) {
+              this.moreInfoData.isCase = result.isCase;
+              this.moreInfoData.id = result.id;
+              this.moreInfoData.pdfLink = result.pdfLink;
+              this.moreInfoData.title = " the Supreme Court Ruling on " + result.title + " (" + result.year + ")" + " that Inspired this Senario";
+              this.moreInfoData.exists = true;
+              this.cdr.detectChanges();
+            } else {
+              this.moreInfoData.title = result.title;
+              this.moreInfoData.description = result.description;
+              this.moreInfoData.exists = true;
+              this.cdr.detectChanges();
+            }
+          } 
+        }
+      }
+    );
+  }
+
+  getUrl():SafeResourceUrl | null{
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.moreInfoData.pdfLink);
+  }
+}
